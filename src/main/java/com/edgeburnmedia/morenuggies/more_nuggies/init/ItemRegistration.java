2eaf9dc1fa6c04cf51e5b85b59dfa8147966ad5a
Edgeburn <edgeburn02@mailredirect.edgeburnmedia.com>
2023-04-19 20:23:47 -0400

    registration of all nuggies

--- src/main/java/com/edgeburnmedia/morenuggies/more_nuggies/init/ItemRegistration.java
+++ src/main/java/com/edgeburnmedia/morenuggies/more_nuggies/init/ItemRegistration.java
@@ -9,17 +9,88 @@ import static com.edgeburnmedia.morenuggies.more_nuggies.MoreNuggies.MODID;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ItemRegistration {
-	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(
+		ForgeRegistries.ITEMS, MODID);
 	private final IEventBus modEventBus;
 
-	public static final RegistryObject<Item> LEATHER_NUGGET = ITEMS.register("leather_nugget", () -> new Item(new Item.Properties().tab(
-		CreativeModeTab.TAB_MATERIALS)));
+
+	public static final RegistryObject<Item> AMETHYST_NUGGET = ITEMS.register("amethyst_nugget",
+		() -> new Item(new Item.Properties().tab(
+			CreativeModeTab.TAB_MATERIALS)));
+
+	public static final RegistryObject<Item> COOKED_CHICKEN_NUGGET = ITEMS.register(
+		"cooked_chicken_nugget", () -> new Item(new Item.Properties().tab(
+			CreativeModeTab.TAB_FOOD)));
+
+	public static final RegistryObject<Item> COAL_NUGGET = ITEMS.register("coal_nugget",
+		() -> new Item(new Item.Properties().tab(
+			CreativeModeTab.TAB_MATERIALS))); // TODO: Add coal nugget as fuel
+
+	public static final RegistryObject<Item> COBBLESTONE_NUGGET = ITEMS.register(
+		"cobblestone_nugget", () -> new Item(new Item.Properties().tab(
+			CreativeModeTab.TAB_MATERIALS)));
+
+	public static final RegistryObject<Item> COPPER_NUGGET = ITEMS.register("copper_nugget",
+		() -> new Item(new Item.Properties().tab(
+			CreativeModeTab.TAB_MATERIALS)));
+
+	public static final RegistryObject<Item> DIAMOND_NUGGET = ITEMS.register("diamond_nugget",
+		() -> new Item(new Item.Properties().tab(
+			CreativeModeTab.TAB_MATERIALS)));
+
+	public static final RegistryObject<Item> ECHO_NUGGET = ITEMS.register("echo_nugget",
+		() -> new Item(new Item.Properties().tab(
+			CreativeModeTab.TAB_MATERIALS)));
+
+	public static final RegistryObject<Item> EMERALD_NUGGET = ITEMS.register("emerald_nugget",
+		() -> new Item(new Item.Properties().tab(
+			CreativeModeTab.TAB_MATERIALS)));
+
+	public static final RegistryObject<Item> LEATHER_NUGGET = ITEMS.register("leather_nugget",
+		() -> new Item(new Item.Properties().tab(
+			CreativeModeTab.TAB_MATERIALS)));
+
+	public static final RegistryObject<Item> NETHERITE_NUGGET = ITEMS.register("netherite_nugget",
+		() -> new Item(new Item.Properties().tab(
+			CreativeModeTab.TAB_MATERIALS)));
+
+	public static final RegistryObject<Item> OBSIDIAN_NUGGET = ITEMS.register("obsidian_nugget",
+		() -> new Item(new Item.Properties().tab(
+			CreativeModeTab.TAB_MATERIALS)));
+
+	public static final RegistryObject<Item> PHANTOM_MEMBRANE_NUGGET = ITEMS.register(
+		"phantom_membrane_nugget", () -> new Item(new Item.Properties().tab(
+			CreativeModeTab.TAB_MATERIALS)));
+
+	public static final RegistryObject<Item> PRISMARINE_NUGGET = ITEMS.register("prismarine_nugget",
+		() -> new Item(new Item.Properties().tab(
+			CreativeModeTab.TAB_MATERIALS)));
+
+	public static final RegistryObject<Item> QUARTZ_NUGGET = ITEMS.register("quartz_nugget",
+		() -> new Item(new Item.Properties().tab(
+			CreativeModeTab.TAB_MATERIALS)));
+
+	public static final RegistryObject<Item> RAW_CHICKEN_NUGGET = ITEMS.register(
+		"raw_chicken_nugget", () -> new Item(new Item.Properties().tab(
+			CreativeModeTab.TAB_FOOD)));
+
+	public static final RegistryObject<Item> REDSTONE_NUGGET = ITEMS.register("redstone_nugget",
+		() -> new Item(new Item.Properties().tab(
+			CreativeModeTab.TAB_MATERIALS)));
+
+	public static final RegistryObject<Item> SCUTE_NUGGET = ITEMS.register("scute_nugget",
+		() -> new Item(new Item.Properties().tab(
+			CreativeModeTab.TAB_MATERIALS)));
+
+	public static final RegistryObject<Item> WOODEN_NUGGET = ITEMS.register("wooden_nugget",
+		() -> new Item(new Item.Properties().tab(
+			CreativeModeTab.TAB_MATERIALS)));
 
 	public ItemRegistration(IEventBus modEventBus) {
 		this.modEventBus = modEventBus;
